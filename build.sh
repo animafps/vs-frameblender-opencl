0efaef15730b03b4f6529142fd19a3cbc4f35c30
atzur <johannes.pihko@gmail.com>
2022-08-31 00:03:33 +0300

    fix memory issues and error check stuff

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
-clang++ ./src/blender.cpp -O3 -march=native -shared -std=c++17 -I./vapoursynth -o blender.so
-sudo mv blender.so /usr/lib/vapoursynth
+clang++ ./src/blender.cpp -O3 -march=native -shared -std=c++17 -I./vapoursynth -o ./blender.so
+
+if [ $? -eq 0 ]; then
+    echo "Build success"
+else
+    echo "Build failed"
+    exit 1
+fi
+
+sudo mv ./blender.so /usr/lib/vapoursynth/blender.so
